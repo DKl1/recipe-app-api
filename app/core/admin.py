98f232b70814f3c42f4835a33a2b1fb07d8bf2cc
Данило Клим <danyloklym@Danilos-Laptop.local>
2024-01-12 15:32:10 +0200

    setup admin pages

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,21 +5,21 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core import models # it's ok
+from core import models  # it's ok
+
 
 class UserAdmin(BaseUserAdmin):
-
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (
             _('Permissions'),
-              {'fields': (
-                  'is_active',
-                  'is_staff',
-                  'is_superuser',
-    )}),
+            {'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )}),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
@@ -37,5 +37,6 @@ class UserAdmin(BaseUserAdmin):
 
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
