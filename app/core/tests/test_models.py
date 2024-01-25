142d82dc732dab3335a1466c2bb67b790a152bed
Данило Клим <danyloklym@Danilos-Laptop.local>
2024-01-25 20:36:19 +0200

    recipe and tags apis

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,14 @@
 """Tests for models."""
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def create_user(email='user@example.com', password='password123'):
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -43,3 +50,19 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            'test@examзle.com',
+            'testpassword',
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
